17af9f2d89e4b5e4560ab163189d8ac7ebaa761b
Isaque Torres <isaquedealmeidatorres@gmail.com>
2025-05-16 14:49:52 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+print ('Bem vindo ao meu primeiro programa de lógica!!!')
+print ('aula toppp!!!')
+print("Avaliação do Operador Lógico E")
+P=input('Digite o valor lógico de P:')    
+Q=input('Digite o valor lógico de Q:')
+if(P == 'True' and Q == 'False'):
+    print("Falso")
+if(P == 'True' and Q == 'True'):
+    print('Verdadeiro')
+if(P == 'False' and Q == 'False'):
+    print("Falso")
+if(P == 'False' and Q == 'True'):
+    print("Falso")    
+
+print('Avaliador Lógico OU')
+P=input('Digite o valor lógico de P')
+Q=input('Digite o valor lògico De Q')
+if(P == 'True' and Q == 'True'):
+    print('Verdadeiro')
+if(P == 'True' and Q == 'False'):
+    print('Verdadeiro')
+if(P == 'False' and Q == 'True'):
+    print('Verdadeiro')
+if(P == 'False' and Q == 'False'):
+    print('Falso')    
+
+print ('Avaliador lógico SE ...ENTÃO')
+P=input('Digite o valor lógico de P')
+Q=input('Digite o valor lògico De Q')
+if(P == 'True' and Q == 'True'):
+    print('Verdadeiro')
+if(P == 'True' and Q == 'False'):
+    print('Falso')
+if(P == 'False' and Q == 'True'):
+    print('Verdadeiro')
+if(P == 'False' and Q == 'False'):
+    print('Verdadeiro')    
+
+print ('Avaliador lógico Se...,e somente se')
+P=input('Digite o valor lógico de P')
+Q=input('Digite o valor lògico De Q')
+if(P == 'True' and Q == 'True'):
+    print('Verdadeiro')
+if(P == 'True' and Q == 'False'):
+    print('Falso')
+if(P == 'False' and Q == 'True'):
+    print('Falso')
+if(P == 'False' and Q == 'False'):
+    print('Verdadeiro')    
